fd3c849ac3cc8bafce3b7a671bca8f23ca57bc15
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:46:26 +0300

    Add files via upload

---
+++ Nesneleri tuşlarla hareket ettime örnek2/Nesneleri tuşlarla hareket ettime örnek2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 77);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(345, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 178);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(78, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 34);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 178);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(78, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 34);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 304);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ Nesneleri tuşlarla hareket ettime örnek2/Nesneleri tuşlarla hareket ettime örnek2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;//pictureboxın x kordinatını tanımlıyoruz
+            int y = pictureBox1.Location.Y;// y kordinatını tanımlıyoruz.
+
+            if (e.KeyCode == Keys.Right && pictureBox1.Right < label1.Left)//eğer klavyeden basılan tuş D ise;
+            {
+                x = x + 5;//x kordinatına doğru 5 pixel git
+            }
+            if (e.KeyCode == Keys.Left && pictureBox1.Left > label3.Right)
+            {
+                x = x - 5;
+            }
+            if (e.KeyCode == Keys.Up && pictureBox1.Top > label2.Bottom)
+            {
+                y = y - 5;
+            }
+            if (e.KeyCode == Keys.Down && pictureBox1.Bottom < label4.Top)
+            {
+                y = y + 5;
+            }
+
+          
+
+            pictureBox1.Location = new Point(x,y);//Burası x,y olmalıdır bu sırayla olmalıdır.
+            if (pictureBox1.Right >= label1.Left)
+            {
+                label1.BackColor = Color.Red;
+            }
+            if (pictureBox1.Top <= label2.Bottom)
+            {
+                label2.BackColor = Color.Yellow;
+            }
+            if (pictureBox1.Left <= label3.Right)
+            {
+                label3.BackColor = Color.Green;
+            }
+            if (pictureBox1.Bottom >= label4.Top)
+            {
+                label4.BackColor = Color.Blue;
+            }
+            
+        }
+    }
+}
